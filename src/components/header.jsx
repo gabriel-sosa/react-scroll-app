499b8343c9e83e76b46e3380edf2604022223ab8
Gabriel <gabe.dev.web@gmail.com>
2019-05-06 21:09:11 +0200

    add initial components

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h1>Asdrubale Lorem Ipsum Dolor</h1>
+        </div>
+    );
+}
---
+++ src/components/jumbotron.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <img src="/images/75720b52593a8db4742aa27e248d2daebb0e0528.png" alt=""/>
+    )
+}
